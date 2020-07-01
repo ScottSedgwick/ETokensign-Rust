dcb8942f66e55c5fdce289b6690a4ffa7578ca46
Scott Sedgwick <ssedgwick@cochlear.com>
2020-07-01 16:43:16 +1000

    Put comment in macros.rs to attribute it more accurately

--- src/cryptuiapi/macros.rs
+++ src/cryptuiapi/macros.rs
@@ -1,3 +1,6 @@
+// This is totally stolen from the Rust winapi crate - just copied here while I try to get the 
+// interface for "CryptUIWizDigitalSign" to work.
+
 // Licensed under the Apache License, Version 2.0
 // <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option.
